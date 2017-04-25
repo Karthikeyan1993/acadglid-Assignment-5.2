ddfb8ad978b44161fa4c7b7ce6b22f826c384d18
Karthikeyan1993 <karthik.infotec99@gmail.com>
2017-04-25 10:00:02 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,15 @@
+var data={};
+var data={
+  name:'Karthikeyan.K',
+  age:24,
+  dob:'22-01-1993',
+  placeofbirth:'Coimbatore'
+};
+// personalInfo will print data in console
+var personalInfo=function functionName() {
+  console.log('Name :'+data.name);
+  console.log('Age :'+data.age);
+  console.log('Date Of Birth :'+data.dob);
+  console.log('Place Of Birth :'+data.placeofbirth);
+}
+personalInfo();
